e0d71f314bf5cb04964713bde66b8814420c60e9
The8472 <git@infinite-source.de>
2017-01-27 21:27:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# ffcnt 
+
+Fast file counting for spinning rust in rust.
+
+ffcnt's sole purpose is to provide a faster alternative to `find /some/path/ -type f | wc -l`.
+It achieves that by looking up the extent map *of directories* and reordering recursion into the directory tree by the physical offset of the first extent of each directory.
+This greatly reduces disk seeks.
+
+
+## Requirements
+
+* Linux
+* A filesystem that supports the `fiemap` ioctl on directories. Otherwise no speedup over `find` will be achieved, but counting will still work. Tested with Ext4, others may work too (Feedback welcome!)
+
+You can test support the `filefrag` utility.  
+
+```
+## supported
+
+$ filefrag /tmp/
+/tmp/: 3 extents found
+
+
+## unsupported
+
+$ filefrag /mnt/test/
+/mnt/test/: FIBMAP unsupported
+```
+
+
+## Unscientific Benchmark
+
+Idle system:
+
+```
+# echo 3 > /proc/sys/vm/drop_caches ; time ffcnt .
+196608
+
+real	0m23.889s
+user	0m1.233s
+sys	0m2.127s
+
+# echo 3 > /proc/sys/vm/drop_caches ; time find . -type f | wc -l
+196608
+
+real	2m31.562s
+user	0m0.557s
+sys	0m3.860s
+```
+
+Busy system with mixed read/write workload:
+
+```
+# echo 3 > /proc/sys/vm/drop_caches ; time ffcnt . 
+4411262
+
+real	10m36.288s
+user	0m3.656s
+sys	0m7.588s
+
+# echo 3 > /proc/sys/vm/drop_caches ; time find . -type f | wc -l
+4412101
+
+real	45m54.955s
+user	0m3.212s
+sys	0m12.044s
+```
+
+Both tests were performed on HDDs
+
+
+## Ideas
+
+* 1 thread per block device in tree
+* filter by name
+* optimized du
---
+++ src/main.rs
@@ -0,0 +1,116 @@
+//   ffcnt
+//   Copyright (C) 2017 The 8472
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software Foundation,
+//   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#![deny(warnings)]
+#![feature(btree_range)]
+#![feature(collections_bound)]
+extern crate btrfs;
+
+use btrfs::linux::{get_file_extent_map_for_path};
+use std::fs::*;
+//use std::os::linux::fs::MetadataExt;
+use std::path::PathBuf;
+use std::collections::BTreeMap;
+use std::collections::Bound::Included;
+use std::error::Error;
+use std::io::Write;
+use std::path::Path;
+
+
+fn scan_dirs(p : &Path) -> std::io::Result<u64> {
+    let mut cnt = 0;
+
+    let mut phy_sorted : BTreeMap<u64, PathBuf> = std::collections::BTreeMap::new();
+    let mut unordered : Vec<PathBuf> = vec![];
+    let mut cursor = 0;
+
+    unordered.push(p.to_owned());
+
+
+    while !phy_sorted.is_empty() || !unordered.is_empty() {
+
+        let next = {
+            let next_key = phy_sorted.range((Included(&cursor), Included(&std::u64::MAX))).next().map(|(k,_)| *k);
+            if let Some(k) = next_key  {
+                phy_sorted.remove(&k)
+            } else {
+                unordered.pop()
+            }
+        };
+
+        match next {
+            Some(p) => {
+
+                match read_dir(&p) {
+                    Ok(dir_iter) => {
+                        for de in dir_iter.filter_map(|de| de.ok()) {
+                            let entry = de.path();
+                            let meta = de.file_type().unwrap();
+                            if meta.is_file() {
+                                cnt+=1;
+                            }
+                            if !meta.is_dir() {
+                                continue;
+                            }
+
+                            //print!{"{} {} ", entry.to_string_lossy(), meta.st_ino()};
+                            match get_file_extent_map_for_path(&entry) {
+                                Ok(ref extents) if !extents.is_empty() => {
+                                    //println!("{:?}", extents);
+                                    if let Some(old) = phy_sorted.insert(extents[0].physical, entry) {
+                                        unordered.push(old);
+                                    }
+                                },
+                                _ => {
+                                    unordered.push(entry);
+                                }
+                            }
+                        }
+                    }
+                    Err(open_err) => {
+                        writeln!(std::io::stderr(), "skipping {} reason: {}", &p.to_string_lossy(), open_err.description())?;
+                    }
+                }
+
+
+            },
+            None => {
+                cursor = 0;
+            }
+        }
+    }
+
+
+    Ok(cnt)
+}
+
+fn process_args() -> std::io::Result<u64> {
+    let root = if let Some(str) = std::env::args().nth(1) {
+        Path::new(&str).to_owned()
+    } else {
+        std::env::current_dir()?
+    };
+    scan_dirs(&root)
+}
+
+
+fn main() {
+
+    match process_args() {
+        Ok(cnt) => {println!("{}", cnt);}
+        Err(e) => {writeln!(std::io::stderr(),"{}", e.description()).unwrap();}
+    };
+}
