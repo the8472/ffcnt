71b2e4087e16a7039bfa162cd38002c4d2bc46b8
The8472 <git@infinite-source.de>
2017-04-05 21:52:45 +0200

    remove stabilized feature flags

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@
 //   along with this program; if not, write to the Free Software Foundation,
 //   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #![deny(warnings)]
-#![feature(btree_range)]
-#![feature(collections_bound)]
 #![feature(alloc_system)]
 extern crate btrfs;
 extern crate alloc_system;
