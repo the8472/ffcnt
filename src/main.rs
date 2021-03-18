33a4ae003a4bfc18d954e54f344022f98060b5b2
The8472 <git@infinite-source.de>
2021-03-18 23:45:56 +0100

    version bump

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@
 //   You should have received a copy of the GNU General Public License
 //   along with this program; if not, write to the Free Software Foundation,
 //   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-#![deny(warnings)]
 #[macro_use] extern crate clap;
 #[macro_use] extern crate derive_error;
 extern crate platter_walk;
